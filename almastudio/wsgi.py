31ff9fbf565208430c5e8d0cbeae2b89f595f69d
Ofer Elzam-Toporik <Oferel1@gmail.com>
2020-05-09 20:58:13 +0300

    Update wsgi.py

--- almastudio/wsgi.py
+++ almastudio/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,8 +15,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'almastudio.settings')
 
 application = get_wsgi_application()
-"""
 
+"""
 #for apache
 import os
 import sys
@@ -25,3 +25,4 @@ os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_proje
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "almastudio.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+"""
