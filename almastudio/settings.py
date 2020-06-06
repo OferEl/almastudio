8c4ac2bbee3d20118c80a50778e761770ada0fcc
Ofer Elzam-Toporik <Oferel1@gmail.com>
2020-06-06 12:28:14 +0300

    a

--- almastudio/settings.py
+++ almastudio/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = 'h7_uupdc_!*&3ok(awqcoi*)@&@6p&dzj6cju)uqz-k@=d-98d'
 #DEBUG = True
 DEBUG = False
 
-#ALLOWED_HOSTS = ['django-labs.herokuapp.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-labs.herokuapp.com']
+#ALLOWED_HOSTS = ['*']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE= True
+CSRF_COOKIE_DOMAIN = 'django-labs.herokuapp.com'
 
 # Application definition
 
