25007f07f1ddb645f9ded11dcb29d4097d4312eb
Ofer Elzam-Toporik <Oferel1@gmail.com>
2020-06-07 19:45:03 +0300

    d

--- almastudio/settings.py
+++ almastudio/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h7_uupdc_!*&3ok(awqcoi*)@&@6p&dzj6cju)uqz-k@=d-98d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['lab4django.herokuapp.com']
 ALLOWED_HOSTS = ['*']
